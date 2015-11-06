1822139d344ac56b81c930f34fe126baf8a54122
Trevor Sherrard <sherrardtr@gmail.com>
2015-11-05 21:41:23 -0500

    first commit

---
+++ test2.py
@@ -0,0 +1,35 @@
+#opens up a webcam feed so you can then test your classifer in real time
+#using detectMultiScale
+import numpy
+import cv2
+
+def detect(img):
+    hammer = cv2.CascadeClassifier("hammer.xml")
+    wire = cv2.CascadeClassifier("wireStripper.xml")
+    hammerRects = hammer.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (275,275))
+    wireRects = wire.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (275,275))
+
+    if (len(hammerRects) == 0 and len(wireRects) == 0):
+        return [], img, ""
+    elif(len(hammerRects) != 0):
+        hammerRects[:, 2:] += hammerRects[:, :2]
+        return hammerRects, img, "hammer"
+    elif(len(wireRects) != 0):
+        wireRects[:, 2:] += wireRects[:, :2]
+        return wireRects, img, "wire Stripper"
+
+def box(rects, img, txt):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+	cv2.putText(img,txt,(x1,y1),  cv2.FONT_HERSHEY_SIMPLEX, .75,(255,0,0),2,10)
+
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, img = cap.read(0)
+    rects, img, detected = detect(img)
+    box(rects, img, detected)
+    cv2.imshow("frame", img)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+	break
