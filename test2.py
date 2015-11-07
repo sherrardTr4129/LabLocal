56f7e5fe5d071558004622324d25212700cca019
Trevor Sherrard <sherrardtr@gmail.com>
2015-11-07 16:09:49 -0500

    added support for hand saw

--- test2.py
+++ test2.py
@@ -5,18 +5,18 @@ import cv2
 
 def detect(img):
     hammer = cv2.CascadeClassifier("hammer.xml")
-    wire = cv2.CascadeClassifier("wireStripper.xml")
-    hammerRects = hammer.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (275,275))
-    wireRects = wire.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (275,275))
+    saw = cv2.CascadeClassifier("saw.xml")
+    hammerRects = hammer.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (335,335))
+    sawRects = saw.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (300,300))
 
-    if (len(hammerRects) == 0 and len(wireRects) == 0):
+    if (len(hammerRects) == 0 and len(sawRects) == 0):
         return [], img, ""
     elif(len(hammerRects) != 0):
         hammerRects[:, 2:] += hammerRects[:, :2]
         return hammerRects, img, "hammer"
-    elif(len(wireRects) != 0):
-        wireRects[:, 2:] += wireRects[:, :2]
-        return wireRects, img, "wire Stripper"
+    elif(len(sawRects) != 0):
+        sawRects[:, 2:] += sawRects[:, :2]
+        return sawRects, img, "hand saw"
 
 def box(rects, img, txt):
     for x1, y1, x2, y2 in rects:
@@ -24,10 +24,10 @@ def box(rects, img, txt):
 	cv2.putText(img,txt,(x1,y1),  cv2.FONT_HERSHEY_SIMPLEX, .75,(255,0,0),2,10)
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while(True):
-    ret, img = cap.read(0)
+    ret, img = cap.read()
     rects, img, detected = detect(img)
     box(rects, img, detected)
     cv2.imshow("frame", img)
