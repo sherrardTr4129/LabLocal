cbfea44848f3b66533a82609d7678851d26bb2fa
Trevor Sherrard <sherrardtr@gmail.com>
2015-11-14 23:44:26 -0500

    added support for second motor

--- StepperDriver/StepperDriver.ino
+++ StepperDriver/StepperDriver.ino
@@ -8,23 +8,36 @@ int INPUT_SIZE = 40;
 
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-AF_Stepper motor(200, 1);
+AF_Stepper motor(200, 2);
+AF_Stepper motor1(48, 1);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
 
-  motor.setSpeed(10);  // 10 rpm   
+  motor.setSpeed(20);  // 10 rpm   
+  motor1.setSpeed(20);  // 10 rpm   
+
 }
 
 void forwardBase(int stepnum)
 {
-  motor.step(stepnum, FORWARD, SINGLE); 
+  motor.step(stepnum, FORWARD, MICROSTEP); 
 }
 
 void reverseBase(int stepnum)
 {
-  motor.step(stepnum, BACKWARD, SINGLE); 
+  motor.step(stepnum, BACKWARD, MICROSTEP); 
+}
+
+void forwardLaser(int stepnum)
+{
+  motor1.step(stepnum, FORWARD, MICROSTEP); 
+}
+
+void reverseLaser(int stepnum)
+{
+  motor1.step(stepnum, BACKWARD, MICROSTEP); 
 }
 
 
@@ -64,6 +77,10 @@ void loop() {
     forwardBase(xcoordinate);
   else if(xcoordinate < 0)
     reverseBase(abs(xcoordinate));
+  if(ycoordinate > 0)
+    forwardLaser(ycoordinate);
+  else if(ycoordinate < 0)
+    reverseLaser(abs(ycoordinate));
   xcoordinate = 0;
   ycoordinate = 0;
 }
