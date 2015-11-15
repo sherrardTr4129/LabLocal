56ce765f3a50036e1e95b33c791ba177e27d7f29
Trevor Sherrard <sherrardtr@gmail.com>
2015-11-14 23:55:17 -0500

    made motors faster

--- StepperDriver/StepperDriver.ino
+++ StepperDriver/StepperDriver.ino
@@ -9,14 +9,14 @@ int INPUT_SIZE = 40;
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
 AF_Stepper motor(200, 2);
-AF_Stepper motor1(48, 1);
+AF_Stepper motor1(200, 1);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
 
-  motor.setSpeed(20);  // 10 rpm   
-  motor1.setSpeed(20);  // 10 rpm   
+  motor.setSpeed(80);  // 10 rpm   
+  motor1.setSpeed(80);  // 10 rpm   
 
 }
 
